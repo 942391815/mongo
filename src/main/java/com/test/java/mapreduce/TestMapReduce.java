b09648445592cafa262938d912f7678cabdc260b
Micheal <Micheal@Micheal-PC>
2016-05-16 20:42:57 +0800

    test java mapreduce

--- src/main/java/com/test/java/mapreduce/TestMapReduce.java
+++ src/main/java/com/test/java/mapreduce/TestMapReduce.java
@@ -17,8 +17,9 @@ public class TestMapReduce {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		MongoTemplate mongoTemplate = (MongoTemplate)context.getBean("mongoTemplate");
+//		mongoTemplate.
 		String mapFunction = "function(){"+
-		"emit(this.tenantId,1);"+
+		"emit(this.name,1);"+
 		"}";
 		String reduceFunction="function(key,values){"+
 		"var cnt=0;"+   
@@ -27,8 +28,8 @@ public class TestMapReduce {
 		"});"+
 		"return cnt;"+
 	"}";
-		MapReduceOutput mapReduce = mongoTemplate.getCollection("templateApproval").mapReduce(mapFunction, reduceFunction, "result", null);
-		mapReduce.getCommand();
+//		MapReduceOutput mapReduce = mongoTemplate.getCollection("user").mapReduce(mapFunction, reduceFunction, "result", null);
+//		mapReduce.getCommand();
 		
 //		var cc = {
 //			    "mapreduce": "templateApproval",
@@ -37,12 +38,12 @@ public class TestMapReduce {
 //			    "out":"result"
 //			}
 		
-//		DBCollection coll = mongoTemplate.getCollection("templateApproval");
-//		MapReduceCommand mapcmd = new MapReduceCommand(coll, mapFunction, reduceFunction,
-//				"result", MapReduceCommand.OutputType.REPLACE, null);
-//		MapReduceOutput cars = coll.mapReduce(mapcmd);
-//		for (DBObject o : cars.results()) {
-//			System.out.println(o.toString());
-//		}
+		DBCollection coll = mongoTemplate.getCollection("user");
+		MapReduceCommand mapcmd = new MapReduceCommand(coll, mapFunction, reduceFunction,
+				"result", MapReduceCommand.OutputType.REPLACE, null);
+		MapReduceOutput cars = coll.mapReduce(mapcmd);
+		for (DBObject o : cars.results()) {
+			System.out.println(o.toString());
+		}
 	}
 }
