736c61463ffae2db1bbdec5f3380cb4a29b2bcd9
Micheal <Micheal@Micheal-PC>
2016-05-16 22:23:55 +0800

    mapReduce

--- src/main/java/com/test/java/mapreduce/TestMapReduce.java
+++ src/main/java/com/test/java/mapreduce/TestMapReduce.java
@@ -1,15 +1,10 @@
 package com.test.java.mapreduce;
 
-import java.util.Set;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.CommandResult;
 import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MapReduceCommand;
 import com.mongodb.MapReduceOutput;
@@ -17,7 +12,6 @@ public class TestMapReduce {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		MongoTemplate mongoTemplate = (MongoTemplate)context.getBean("mongoTemplate");
-//		mongoTemplate.
 		String mapFunction = "function(){"+
 		"emit(this.name,1);"+
 		"}";
