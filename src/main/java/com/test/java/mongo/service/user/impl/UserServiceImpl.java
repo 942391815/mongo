d97c8c435ac61bf1df63af0cdb23b1add248bfe6
micheal <942391815@qq.com>
2016-06-07 13:26:45 +0800

    添加user

---
+++ src/main/java/com/test/java/mongo/service/user/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.test.java.mongo.service.user.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.test.java.dto.User;
+import com.test.java.mongo.base.BaseServiceImpl;
+import com.test.java.mongo.service.user.UserService;
+
+@Service
+public class UserServiceImpl extends BaseServiceImpl<User> implements UserService{
+
+}
